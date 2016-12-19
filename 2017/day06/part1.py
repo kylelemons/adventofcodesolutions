83335bbd745f848f4bb22aaf1aae8db473f2dd4c
Kyle Lemons <kyle@kylelemons.net>
2016-12-18 20:41:21 -0800

    day06

---
+++ 2017/day06/part1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2.7
+
+from itertools import groupby
+
+def decode(lines):
+    cols = zip(*lines)
+    scols = [sorted(col) for col in cols]
+    freqs = [[(len(list(v)), k) for k, v in groupby(col)] for col in scols]
+    letters = [sorted(col, reverse=True)[0][1] for col in freqs]
+    return ''.join(letters)
+
+print decode("""eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar""".splitlines())
+
+print decode(open('input.txt').readlines())
