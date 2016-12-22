5e5967a78463c8a9573bb922c7e5c15d5b255f7a
Kyle Lemons <kyle@kylelemons.net>
2016-12-21 21:58:53 -0800

    add template

---
+++ 2017/day19/functional.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+
+import re
+import itertools
+import functools
+import md5
+import math
+
+def answer():
+    pass
+
+tests = [
+    ((), None)
+]
+for args, ans in tests:
+    got = answer(*args)
+    print args, got, ans, got == ans
