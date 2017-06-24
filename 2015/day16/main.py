5f38c8f6c629083ba50b1b5b47f933c67630d677
Kyle Lemons <kyle@kylelemons.net>
2017-06-24 12:54:24 -0700

    Fix python style

--- 2015/day16/main.py
+++ 2015/day16/main.py
@@ -1,20 +1,22 @@
+'''adventofcode 2015 day16'''
+
 import re
 
-clues = {k: int(v) for k, v in re.findall(r'(\w+): (\d+)', open("clues.txt").read())}
+CLUES = {k: int(v) for k, v in re.findall(r'(\w+): (\d+)', open("clues.txt").read())}
 
-sues = {s: {k: int(v) for k, v in re.findall(r'(\w+): (\d+)', c)}
+SUES = {s: {k: int(v) for k, v in re.findall(r'(\w+): (\d+)', c)}
         for s, c in re.findall(r'Sue (\d+): (.*)', open("input.txt").read())}
 
-for sue, info in sues.iteritems():
+for sue, info in SUES.iteritems():
     matches = 0
     for k, v in info.iteritems():
         if k == "cats" or k == "trees":
-            if clues[k] < v:
+            if CLUES[k] < v:
                 matches += 1
         elif k == "goldfish" or k == "pomeranians":
-            if clues[k] > v:
+            if CLUES[k] > v:
                 matches += 1
-        elif clues[k] == v:
+        elif CLUES[k] == v:
             matches += 1
     if matches == len(info):
         print sue, info
