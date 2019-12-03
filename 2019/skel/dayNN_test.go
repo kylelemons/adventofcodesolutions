3efc78bb3ae20263be0f61864c927efad08e2c33
Kyle Lemons <kyle@kylelemons.net>
2019-12-02 22:21:32 -0800

    2019: Add skeleton

---
+++ 2019/skel/dayNN_test.go
@@ -0,0 +1,48 @@
+// Copyright 2018 Kyle Lemons
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package acoday is the entrypoint for this AoC solution.
+package acoday
+
+import (
+	"testing"
+
+	"github.com/kylelemons/adventofcodesolutions/2019/advent"
+)
+
+func part1(t *testing.T, in string) (ret int) {
+	advent.Lines(t, in, func() {
+		// ...
+	})
+	return
+}
+
+func TestPart1(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want int
+	}{
+		{"part1 example 0", "...", 0},
+		// {"part1 answer", advent.ReadFile(t, "input.txt"), -1},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got, want := part1(t, test.in), test.want; got != want {
+				t.Errorf("part1(%#v)\n = %#v, want %#v", test.in, got, want)
+			}
+		})
+	}
+}
