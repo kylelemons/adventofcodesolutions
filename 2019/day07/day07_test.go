689da9642f55bc73b668ac1822463f26d293d34d
Kyle Lemons <kyle@kylelemons.net>
2020-12-12 23:24:44 -0800

    Fix copyright years

--- 2019/day07/day07_test.go
+++ 2019/day07/day07_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Kyle Lemons
+// Copyright 2019 Kyle Lemons
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
