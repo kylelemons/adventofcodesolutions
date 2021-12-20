9a79e46d22e48b25a3d38c75f85d9e16faf6abff
Kyle Lemons <kyle@kylelemons.net>
2021-12-19 23:36:54 -0800

    Fix date

--- skel/dayNN_test.go
+++ skel/dayNN_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Kyle Lemons
+// Copyright 2021 Kyle Lemons
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
