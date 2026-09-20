bf1389290688ea8177fe68ed1278d22e50d21dd9
Kyle Lemons <kyle@kylelemons.net>
2026-09-20 15:36:11 -0700

    2025 day08 part2

--- 2025/day08/day08_test.go
+++ 2025/day08/day08_test.go
@@ -162,3 +162,92 @@ func TestPart1(t *testing.T) {
 		})
 	}
 }
+
+func part2(t *testing.T, in string, n int) (ret int) {
+	input := parseInput(t, in)
+
+	type Dist struct {
+		A, B Loc
+		Dist float64
+	}
+	var dists []Dist
+	for i, a := range input.Boxes {
+		for _, b := range input.Boxes[i+1:] {
+			if a == b {
+				continue
+			}
+			dists = append(dists, Dist{a, b, a.Dist(b)})
+		}
+	}
+
+	slices.SortFunc(dists, func(a, b Dist) int {
+		return cmp.Or(
+			cmp.Compare(a.Dist, b.Dist),
+			cmp.Compare(a.A[0], b.A[0]),
+			cmp.Compare(a.A[1], b.A[1]),
+			cmp.Compare(a.A[2], b.A[2]),
+			cmp.Compare(a.B[0], b.B[0]),
+			cmp.Compare(a.B[1], b.B[1]),
+			cmp.Compare(a.B[2], b.B[2]),
+		)
+	})
+
+	// disjoint set forest
+	roots := make(map[Loc]struct{})
+	rootOf := make(map[Loc]Loc)
+	// everything starts in its own set
+	for _, box := range input.Boxes {
+		roots[box] = struct{}{}
+		rootOf[box] = box
+	}
+	// joining two forests
+	join := func(a, b Loc) (joined bool) {
+		a = rootOf[a]
+		b = rootOf[b]
+		if a == b {
+			return false
+		}
+
+		rootOf[b] = a
+		delete(roots, b)
+
+		for box, root := range rootOf {
+			if root == b {
+				rootOf[box] = a
+			}
+		}
+		return true
+	}
+
+	for i := range dists {
+		if n <= 0 {
+			break
+		}
+		a, b := dists[i].A, dists[i].B
+		join(a, b)
+		if len(roots) == 1 {
+			return a[0] * b[0]
+		}
+	}
+	panic("unreachable")
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		n    int
+		want int
+	}{
+		{"part2 example 0", "162,817,812\n57,618,57\n906,360,560\n592,479,940\n352,342,300\n466,668,158\n542,29,236\n431,825,988\n739,650,466\n52,470,668\n216,146,977\n819,987,18\n117,168,530\n805,96,715\n346,949,466\n970,615,88\n941,993,340\n862,61,35\n984,92,344\n425,690,689", 10, 25272},
+		{"part2 answer", advent.ReadFile(t, "input.txt"), 1000, 8141888143},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got, want := part2(t, test.in, test.n), test.want; got != want {
+				t.Errorf("part2(%#v)\n = %#v, want %#v", test.in, got, want)
+			}
+		})
+	}
+}
