4cb5c954e3eb18575cef599e85c54c32cac0533e
Kyle Lemons <kyle@kylelemons.net>
2026-09-20 21:48:20 -0700

    2025 day09 part2

--- 2025/day09/day09_test.go
+++ 2025/day09/day09_test.go
@@ -16,6 +16,7 @@
 package aocday
 
 import (
+	"iter"
 	"testing"
 
 	"github.com/kylelemons/adventofcodesolutions/advent"
@@ -69,6 +70,102 @@ func TestPart1(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			if got, want := part1(t, test.in), test.want; got != want {
 				t.Errorf("part1(%#v)\n = %#v, want %#v", test.in, got, want)
+			}
+		})
+	}
+}
+
+func loopPairs(tiles []Tile) iter.Seq2[Tile, Tile] {
+	return func(yield func(Tile, Tile) bool) {
+		for i := 0; i+1 < len(tiles); i++ {
+			if !yield(tiles[i], tiles[i+1]) {
+				return
+			}
+		}
+		yield(tiles[len(tiles)-1], tiles[0]) // close the loop
+	}
+}
+
+func edgePiercesRect(minR, maxR, minC, maxC int, e0, e1 Tile) bool {
+	if e0.C == e1.C {
+		// horizontal line; reverse rows and columns so we can just do the vertical line logic
+		minR, maxR, minC, maxC = minC, maxC, minR, maxR
+		e0.R, e0.C, e1.R, e1.C = e0.C, e0.R, e1.C, e1.R
+	}
+	r := e0.R
+	cLo, cHi := min(e0.C, e1.C), max(e0.C, e1.C)
+	return minR < r && r < maxR && cHi > minC && cLo < maxC
+}
+
+func pointInPolygon(r, c float64, tiles []Tile) bool {
+	inside := false
+	for e0, e1 := range loopPairs(tiles) {
+		if e0.C == e1.C {
+			edgeC := float64(e0.C)
+			rLo := float64(min(e0.R, e1.R))
+			rHi := float64(max(e0.R, e1.R))
+			if edgeC > c && rLo < r && r < rHi {
+				inside = !inside
+			}
+		}
+	}
+	return inside
+}
+
+func rectInPolygon(minR, maxR, minC, maxC int, tiles []Tile) bool {
+	if minR == maxR && minC == maxC {
+		return true
+	}
+	if minR == maxR {
+		return pointInPolygon(float64(minR)+0.5, float64(minC)+0.5, tiles) ||
+			pointInPolygon(float64(minR)-0.5, float64(minC)+0.5, tiles)
+	}
+	if minC == maxC {
+		return pointInPolygon(float64(minR)+0.5, float64(minC)+0.5, tiles) ||
+			pointInPolygon(float64(minR)+0.5, float64(minC)-0.5, tiles)
+	}
+	return pointInPolygon(float64(minR)+0.5, float64(minC)+0.5, tiles)
+}
+
+func part2(t *testing.T, in string) (ret int) {
+	input := parseInput(t, in)
+
+	for i, t0 := range input.Tiles {
+	nextTile:
+		for _, t1 := range input.Tiles[i+1:] {
+			minR, maxR := min(t0.R, t1.R), max(t0.R, t1.R)
+			minC, maxC := min(t0.C, t1.C), max(t0.C, t1.C)
+			for e0, e1 := range loopPairs(input.Tiles) {
+				if edgePiercesRect(minR, maxR, minC, maxC, e0, e1) {
+					continue nextTile
+				}
+			}
+			if rectInPolygon(minR, maxR, minC, maxC, input.Tiles) {
+				area := (maxR - minR + 1) * (maxC - minC + 1)
+				if area > ret {
+					ret = area
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want int
+	}{
+		{"part2 example 0", "7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3", 24},
+		{"part2 answer", advent.ReadFile(t, "input.txt"), 1566935900},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got, want := part2(t, test.in), test.want; got != want {
+				t.Errorf("part2(%#v)\n = %#v, want %#v", test.in, got, want)
 			}
 		})
 	}
